2e9dc8a0568b5deaa7f7f96bb3c799d4be2c5561
ASTeterin <at28813905@gmail.com>
2023-10-02 22:37:05 +0300

    сделал 3 задание

---
+++ lab2/lab3/Observer.h
@@ -0,0 +1,83 @@
+﻿#pragma once
+
+#include <set>
+#include <functional>
+#include <map>
+
+/*
+Шаблонный интерфейс IObserver. Его должен реализовывать класс, 
+желающий получать уведомления от соответствующего IObservable
+Параметром шаблона является тип аргумента,
+передаваемого Наблюдателю в метод Update
+*/
+template <typename T>
+class IObserver
+{
+public:
+	virtual void Update(T const& data) = 0;
+	virtual ~IObserver() = default;
+};
+
+/*
+Шаблонный интерфейс IObservable. Позволяет подписаться и отписаться на оповещения, а также
+инициировать рассылку уведомлений зарегистрированным наблюдателям.
+*/
+template <typename T>
+class IObservable
+{
+public:
+	virtual ~IObservable() = default;
+	virtual void RegisterObserver(IObserver<T> & observer, int priority) = 0;
+	virtual void NotifyObservers() = 0;
+	virtual void RemoveObserver(IObserver<T> & observer) = 0;
+};
+
+// Реализация интерфейса IObservable
+template <class T>
+class CObservable : public IObservable<T>
+{
+public:
+	typedef IObserver<T> ObserverType;
+
+	void RegisterObserver(ObserverType& observer, int priority) override
+	{
+		if (m_observers.find(&observer) == m_observers.end())
+		{
+			m_observersMap.insert({ priority, &observer });
+			m_observers.insert(&observer);
+			return;
+		}
+
+		throw std::runtime_error("Observer already reristred.");
+	}
+
+	void NotifyObservers() override
+	{
+		T data = GetChangedData();
+		for (auto iter = m_observersMap.rbegin(); iter != m_observersMap.rend(); ++iter)
+		{
+			iter->second->Update(data);
+		}
+	}
+
+	void RemoveObserver(ObserverType & observer) override
+	{
+		for (auto iter = m_observersMap.begin(); iter != m_observersMap.end(); ++iter)
+			if (iter->second == &observer)
+			{
+				m_observersMap.erase(iter->first);
+				return;
+			}
+
+		m_observers.erase(&observer);
+	}
+
+protected:
+	// Классы-наследники должны перегрузить данный метод, 
+	// в котором возвращать информацию об изменениях в объекте
+	virtual T GetChangedData()const = 0;
+
+private:
+	std::set<ObserverType *> m_observers;
+	std::multimap<int, ObserverType*> m_observersMap;
+};
---
+++ lab2/lab3/task3.cpp
@@ -0,0 +1,40 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../../../Catch2/catch.hpp"
+#include "WeatherData.h"
+
+class CMockDisplay : public IObserver<SWeatherInfo>
+{
+public:
+	CMockDisplay(std::string name, std::vector<std::string>& priorities)
+		: m_name(name)
+		, m_priorities(&priorities)
+	{
+	}
+private:
+	void Update(SWeatherInfo const& data) override
+	{
+		m_priorities->push_back(m_name);
+	}
+
+	std::string m_name;
+	std::vector<std::string>* m_priorities;
+};
+
+SCENARIO("test priority")
+{
+	CWeatherData wd;
+
+	std::vector<std::string> priorities;
+	CMockDisplay display1("test1", priorities);
+	CMockDisplay display2("test2", priorities);
+	CMockDisplay display3("test3", priorities);
+	wd.RegisterObserver(display1, 1);
+	wd.RegisterObserver(display2, 2);
+	wd.RegisterObserver(display3, 10);
+
+	wd.SetMeasurements(20, 0.7, 760);
+
+	REQUIRE(priorities[0] == "test3");
+	REQUIRE(priorities[1] == "test2");
+	REQUIRE(priorities[2] == "test1");
+}
